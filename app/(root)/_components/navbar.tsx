21510ab5e866d9bc66a884df16b00a4f85f1229c
shahzod islomov <shahzodislomov39@gmail.com>
2025-06-16 13:13:51 +0500

    mobile version added

--- app/(root)/_components/navbar.tsx
+++ app/(root)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import React from "react";
 import GloablSearch from "./global-search";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import Mobile from "./mobile";
 
 const Navbar = () => {
    const pathName = usePathname()
@@ -15,7 +16,7 @@ const Navbar = () => {
     <div className="h-[10vh] backdrop-blur-sm border-b fixed z-40 inset-0 bg-background">
       <div className="container max-w-6xl mx-auto h-[10vh] w-full flex items-center justify-between">
         <Link href={"/"}>
-          <h1 className="text-4xl font-mono">Wenaco</h1>
+          <h1 className="text-4xl font-mono hidden md:flex">Wenaco</h1>
         </Link>
         <div className="gap-2 hidden md:flex">
           {navLinks.map((nav) => (
@@ -33,6 +34,7 @@ const Navbar = () => {
         <div className="flex items-center gap-1">
           <ModeToggle />
           <GloablSearch />
+          <Mobile />
         </div>
       </div>
     </div>
