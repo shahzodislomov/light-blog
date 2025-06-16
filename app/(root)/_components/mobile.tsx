d0384eb66a91f978e43be64d68279a96e8b6b069
shahzod islomov <shahzodislomov39@gmail.com>
2025-06-16 13:16:57 +0500

    mobile version fixed

--- app/(root)/_components/mobile.tsx
+++ app/(root)/_components/mobile.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -17,40 +17,45 @@ import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 
 const Mobile = () => {
-   const [open,setOpen] = useState(false)
-   const pathName = usePathname()
+  const [open, setOpen] = useState(false);
+  const pathName = usePathname();
 
-   const handleClickClose = () => {
-      setOpen(false)
-   }
+  const handleClickClose = () => {
+    setOpen(false);
+  };
   return (
-      <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger asChild className="flex md:hidden">
-         <Button size={'icon'} variant={'ghost'}>
-         <Menu />
-         </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-         <SheetHeader>
-            <SheetTitle className="sr-only">
-              navigation
-            </SheetTitle>
-         </SheetHeader>
-            <Link href={'/'}>
-               <h1 className="text-4xl font-mono">wenaco</h1>
-            </Link> 
-            <Separator className="my-3"/>
-            <div className="flex flex-col space-y-3">
-               {navLinks.map(nav => (
-                  <Link key={nav.route} href={nav.route} className={cn("hover:bg-blue-400/20 py-1 px-3 cursor-pointer rounded-sm transition-colors",
-                     pathName === nav.route && 'text-blue-400 bg-blue-400/20'
-                  )} onClick={handleClickClose}>
-                     {nav.name}
-                  </Link>
-               ))}
-            </div>
-         </SheetContent>
-      </Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild className="flex md:hidden">
+        <Button size={"icon"} variant={"ghost"}>
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle className="sr-only">navigation</SheetTitle>
+        </SheetHeader>
+        <Link href={"/"}>
+          <h1 className="text-4xl font-mono">wenaco</h1>
+        </Link>
+        <Separator className="my-3" />
+        <div className="flex flex-col space-y-3">
+          {navLinks.map((nav) => (
+            <Link
+              key={nav.route}
+              href={nav.route}
+              className={cn(
+                "hover:bg-blue-400/20 py-1 px-3 cursor-pointer rounded-sm transition-colors flex gap-2",
+                pathName === nav.route && "text-blue-400 bg-blue-400/20"
+              )}
+              onClick={handleClickClose}
+            >
+               <nav.icon className="w-5 h-5"/>
+              {nav.name}
+            </Link>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };
 
